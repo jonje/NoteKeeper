26987aec26343afd5155a6cdc3f9e65a514c8ed7
Jonathan Jensen <haiyun211@gmail.com>
2023-01-18 18:58:54 -0700

    Updates to testing

--- app/src/test/java/org/haknet/notekeeper/ExampleUnitTest.java
+++ app/src/test/java/org/haknet/notekeeper/ExampleUnitTest.java
@@ -1,17 +1,43 @@
 package org.haknet.notekeeper;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    @Spy
+    public MainActivity activity;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testSpy() {
+        assertNotNull(activity);
     }
 }
